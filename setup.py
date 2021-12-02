089b725acde67e2c3eb6040cce606b5e8842253d
BEFH <fultonh1@gmail.com>
2021-12-02 16:09:31 -0500

    update for 0.5.1 with hardcoded version in yaml

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
+import setuptools
 
-"""A simple python script to search for allele switches, strand flips,
-multiallelic sites, ambiguous sites, and indels. The output is in the form of
-a .bim-like table and a log file.
-
-See:
-https://github.com/BEFH/flippyr
-"""
-
-from setuptools import setup, find_packages
-
-setup(
+setuptools.setup(
     name='flippyr',
-    version='0.5.2',
+    version='0.5.1',
     description='Find reference mismatches in PLINK filesets and fix them.',
-    long_description=('A simple python script tosearch for allele switches,\n'
+    long_description=('A simple python script to search for allele switches,\n'
                       'strand flips, multiallelic sites, ambiguous sites,\n'
                       'and indels. The output is in the form of a .bim-like\n'
                       'table and a log file.'),
