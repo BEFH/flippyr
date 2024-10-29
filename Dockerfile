6a720e143c944f2a867449e12805da9614788f18
Brian Fulton-Howard <brian.fulton-howard@mssm.edu>
2024-10-29 11:53:19 -0400

    fix: add base env to path in Docker

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN micromamba install -y -n base -c conda-forge -c bioconda -c bfh \
        python=3.12.7 \
        pyfaidx=0.8.1.3 \
        biopython=1.84 \
-       flippyr=0.6.0 \
+       flippyr=0.6.1 \
        plink=1.90b6.21 && \
     micromamba clean --all --yes
+ENV PATH "$MAMBA_ROOT_PREFIX/bin:$PATH"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='flippyr',
-    version='0.6.0',
+    version='0.6.1',
     description='Find reference mismatches in PLINK filesets and fix them.',
     long_description=('A simple python script to search for allele switches,\n'
                       'strand flips, multiallelic sites, ambiguous sites,\n'
